15f11cc1b5b0f4389f8a11127fde634b024e4bc1
QL-Anton <antohakiselev@yandex.ru>
2017-09-15 01:10:27 +0500

    Добавлена функция для вычисления площади

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,6 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
 public static void main(String[] args){
-System.out.println("Hello, world!");
+double a=0.8;
+
+  System.out.println(area(a));
+
+
   }
+
+
+  public static double area(double len){
+     return len*len;
+      }
 }
