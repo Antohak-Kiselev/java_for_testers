ee5befebe3489ad862afa9c38a121a257c329087
QL-Anton <antohakiselev@yandex.ru>
2017-09-15 20:54:26 +0500

    Добавлены объекты и методы
    Для вычисления площадей прямоугольника и квадрата

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -2,16 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
 
-public static void main(String[] args){
-double a=0.8;
+  public static void main(String[] args) {
 
-  System.out.println(area(a));
+    Square s = new Square(345);
+
+    System.out.println("Площадь квадрата со стороной= " + s.l + " равна " + s.area());
 
 
+    Rectangle r=new Rectangle(234,434);
+
+    System.out.println("Площадь прямоугольника со сторонами  " + r.a + " и " + r.b + " равна " + r.area());
   }
 
 
-  public static double area(double len){
-     return len*len;
-      }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Антон on 15.09.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l=l;
+  }
+
+  public double area(){
+    return this.l*this.l;
+  }
+}
