a6e753ce570fd9f59298cf122e8e213f591f077e
QL-Anton <antohakiselev@yandex.ru>
2017-09-15 21:34:55 +0500

    Добавлена функция для вычисления расстояния между 2мя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -12,7 +12,19 @@ public class MyFirstProgramm {
     Rectangle r=new Rectangle(234,434);
 
     System.out.println("Площадь прямоугольника со сторонами  " + r.a + " и " + r.b + " равна " + r.area());
+
+    Point p1=new Point(0,0);
+    Point p2=new Point(2,2);
+    System.out.println("Distance from p1 to p2 equals  " +distanse(p1,p2));
+
+
   }
 
+
+  public static double distanse(Point p1, Point p2){
+
+    return  Math.sqrt( (p2.a-p1.a)*(p2.a-p1.a) +  (p2.b-p1.b)*(p2.b-p1.b));
+
+  }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Антон on 15.09.2017.
+ */
+public class Point {
+  public double a;
+  public double b;
+
+  public Point(double a, double b){
+    this.a=a;
+    this.b=b;
+  }
+
+}
+
