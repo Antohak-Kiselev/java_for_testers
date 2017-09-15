1e5e840588985a57d0f57c3f12f707b05f8f5b3a
QL-Anton <antohakiselev@yandex.ru>
2017-09-16 01:42:17 +0500

    Подключена новая библиотека и добавлен тест
    Подключена новая библиотека testNG 6.11
    Добавлен тест для проверки вычисления расстояни между двумя точками.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Антон on 16.09.2017.
+ */
+public class SquareTests {
+
+
+@Test
+  public void testArea(){
+    Square s=new Square(7);
+
+  Assert.assertEquals( s.area(),49.0);
+
+  Point p1=new Point(0,0);
+  Point p2=new Point(0,0);
+
+  Assert.assertEquals(p1.distanceToPoint(p2),0.0);
+
+  Point p3=new Point(0,0);
+  Point p4=new Point(4,3);
+
+  Assert.assertEquals(p3.distanceToPoint(p4),5.0);
+
+
+
+
+
+  }
+}
