50c849a152be78b9bc3d0c49771d283d7e116b77
QL-Anton <antohakiselev@yandex.ru>
2017-09-22 19:47:46 +0500

    Реализованна двухуровневая архитекутра тестового набора
    Вспомогательные методы вынесены в отдельные классы.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Антон on 22.09.2017.
+ */
+public class SessionHelper {
+  private FirefoxDriver wd;
+
+  public SessionHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.id("content")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+}
