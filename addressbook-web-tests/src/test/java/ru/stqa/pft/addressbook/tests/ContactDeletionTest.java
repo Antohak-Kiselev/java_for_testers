68217a4ad5033bbdc382c2f673adc2bad2151d21
QL-Anton <antohakiselev@yandex.ru>
2017-10-02 23:12:35 +0500

    Реализованна проверка и обеспечение предусловий для  тестов для модификации и удаления контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
  * Created by Антон on 23.09.2017.
@@ -9,6 +10,11 @@ public class ContactDeletionTest extends  TestBase {
 
  @Test
   public void testContactDeletion(){
+app.getNavigationHelper().gotoHomePage();
+  if (!app.getContactHelper().isThereAContact()){
+   app.getContactHelper().createContact(new ContactData("erer","sdfdsf","sdfdsf","78787","sdfsadf@dsf","test1"));
+
+  }
    app.getContactHelper().setContact();
    app.getContactHelper().deleteSelectedContact();
    app.getContactHelper().returnToHomePage();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -6,12 +6,17 @@ import ru.stqa.pft.addressbook.model.ContactData;
 /**
  * Created by Антон on 23.09.2017.
  */
-public class ContactModificationTests extends  TestBase {
+public class ContactModificationTests extends TestBase {
 
   @Test
-  public  void testContactModification(){
-   app.getContactHelper().setContact();
-    app.getContactHelper().fillContactForm(new ContactData("testfn_mod", "testln_mod", "testad_mod", "91799992", "test@test.test","null"),false);
+  public void testContactModification() {
+
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("erer", "sdfdsf", "sdfdsf", "78787", "sdfsadf@dsf", "test1"));
+
+    }
+    app.getContactHelper().setContact();
+    app.getContactHelper().fillContactForm(new ContactData("testfn_mod", "testln_mod", "testad_mod", "91799992", "test@test.test", "null"), false);
     app.getContactHelper().submitModificationContact();
     app.getNavigationHelper().gotoHomePage();
   }
