1638754b06d341ca992bde327051007284f6662f
QL-Anton <antohakiselev@yandex.ru>
2017-10-10 00:29:15 +0500

    Реализовано сравнение списков в тестах для модификации, создания, удаления групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -5,7 +5,54 @@ public class GroupData {
   private final String header;
   private final String footer;
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private  int id;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + id;
+    return result;
+  }
+
+  @Override
+
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
+
+  public int  getId() {
+    return id;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id=id;
+
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
   public GroupData(String name, String header, String footer) {
+    this.id=0;
+
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -22,4 +69,5 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
 }
