bd78f25c280395b3634c209d5d39ae3da89cb5fd
QL-Anton <antohakiselev@yandex.ru>
2017-10-10 21:29:05 +0500

    Добавлена сортировка списков перед сравнение в тестах для модификации и создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -12,24 +12,6 @@ public class GroupData {
   private  int id;
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = name != null ? name.hashCode() : 0;
-    result = 31 * result + id;
-    return result;
-  }
-
-  @Override
 
   public String toString() {
     return "GroupData{" +
@@ -42,6 +24,21 @@ public class GroupData {
     return id;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
   public GroupData(int id, String name, String header, String footer) {
     this.id=id;
 
@@ -51,7 +48,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id=0;
+    this.id=Integer.MAX_VALUE;
 
     this.name = name;
     this.header = header;
