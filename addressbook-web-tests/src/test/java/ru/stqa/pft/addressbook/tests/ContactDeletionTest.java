6cc1be3bd877023502e2a13b866e25ce6c196c45
QL-Anton <antohakiselev@yandex.ru>
2017-10-11 01:04:48 +0500

    Реализовано сравнение списков до удаления  и после удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -27,5 +27,7 @@ app.getNavigationHelper().gotoHomePage();
    List<ContactData> after=app.getContactHelper().getContactList();
    Assert.assertEquals(after.size(),before.size()-1);
 
+   before.remove(before.size()-1);
+   Assert.assertEquals(before,after);
  }
 }
