98aea6b493fdf8dd21b130dfd1967b80c928ee59
QL-Anton <antohakiselev@yandex.ru>
2017-10-15 01:18:34 +0500

    Улучшен внешний вид проверок для групп с использованием  Hamcrest
    Кроме того используется библиотека guava

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,8 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Антон on 22.09.2017.
@@ -24,17 +29,17 @@ public class GroupModificationTest extends TestBase {
 
   @Test
   public void  testGroupModification(){
-    Set<GroupData> before=app.group().all();
+    Groups before=app.group().all();
     GroupData modifiedGroup=before.iterator().next();
     GroupData group=new GroupData().
             withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
     app.group().modify(group);
-    Set<GroupData> after=app.group().all();
-    Assert.assertEquals(before.size(),after.size());
-    before.remove(modifiedGroup);
-    before.add(group);
+    Groups after=app.group().all();
+    assertEquals(after.size(),before.size());
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
 
-    Assert.assertEquals(before,after);
+
+   
 
 
   }
