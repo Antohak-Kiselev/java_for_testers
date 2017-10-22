90b0bca30005524e48be4f45b976a9c956d3fb5c
QL-Anton <antohakiselev@yandex.ru>
2017-10-22 12:36:20 +0300

    Добавлена работа с опциями командной строки
    Добавлена работа с опциями для генератора с ипользованием jcommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -8,22 +8,41 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
 
 public class GroupDataGenerator {
+  @Parameter (names="-c", description = "Group count")
+   public int count;
+
+  @Parameter (names="-f", description = "Target file")
+   public String file;
+
+
   public static void main(String[] args) throws IOException {
-    System.out.println(new File(".").getAbsolutePath());
+    GroupDataGenerator generator=new GroupDataGenerator();
+       JCommander jCommander = new JCommander(generator);
+        try{
+          jCommander.parse(args);}
+        catch(ParameterException ex) {
 
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
-
+                   jCommander.usage();
+            return;
+          }
+    generator.run();
   }
 
 
+
+  private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+  }
+
   //Запись в файл
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private  void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
@@ -34,7 +53,7 @@ public class GroupDataGenerator {
 
 
   //Генератор данных
-  private static List<GroupData> generateGroups(int count) {
+  private  List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("test %s", i)).
