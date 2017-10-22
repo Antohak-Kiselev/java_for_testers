9cdba4b0c48c1d0b7b3bd8c25755a07720973895
QL-Anton <antohakiselev@yandex.ru>
2017-10-22 11:50:19 +0300

    Добавлена возможность добалвения фотографии при создании контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,4 +1,5 @@
 package ru.stqa.pft.addressbook.model;
+import java.io.File;
 
 public class ContactData {
   private  int  id=Integer.MAX_VALUE;
@@ -15,6 +16,16 @@ public class ContactData {
   private  String email3;
   private String allEmails;
   private  String allAddress;
+  private File photo;
+
+  public File getPhoto() {
+    return photo;
+  }
+
+  public ContactData withPhoto(File photo) {
+    this.photo = photo;
+    return this;
+  }
 
   public String getAllAddress() {
     return allAddress;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.testng.Assert.assertEquals;
@@ -15,8 +17,10 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() {
     app.goTo().HomePage();
    Contacts before = app.contact().all();
+    File photo=new File("src/test/resources/stru.png");
     ContactData contact = new ContactData ().withFirstName("test_First_Name").withLastName("test_Last_name").
-            withAddress("addressTest").withEmail("mail@ru.ru").withMobilePhone("89889934").withGroup("test1");
+            withAddress("addressTest").withEmail("mail@ru.ru").withMobilePhone("89889934").withPhoto(photo).
+            withGroup("test1");
     app.contact().create(contact);
     assertEquals(app.contact().count(), before.size() + 1);
     Contacts after = app.contact().all();
@@ -27,6 +31,8 @@ public class ContactCreationTests extends TestBase {
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c)->c.getId()).max().getAsInt()))));
   }
+
+
 
 
 }
